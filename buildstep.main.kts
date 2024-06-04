11c4fb13be1c62bf328e540adec040399dbdbae5
Mikhail Rodionychev <tralllgm@gmail.com>
2024-06-04 14:49:07 +0200

    if in buildstep

--- buildstep.main.kts
+++ buildstep.main.kts
@@ -1,1 +1,8 @@
-println("##teamcity[buildProblem description='Build problem from kotlin script build step']")
+import java.io.File
+
+var jarFile = File("find-plugin/build/libs/find.jar")
+if (jarFile.exists()) {
+    println("##teamcity[buildProblem description='Jar file with plugin was created successfully']")
+} else {
+    println("##teamcity[buildProblem description='Plugin could not be found']")
+}
