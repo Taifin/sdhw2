d759477412d5e3fac8ae2df70e2c342d90b4a25b
Mikhail Rodionychev <tralllgm@gmail.com>
2024-06-05 13:47:21 +0200

    add two root flows & change buildproblem to message if jar is found

--- buildstep.main.kts
+++ buildstep.main.kts
@@ -5,9 +5,15 @@ println("##teamcity[message text='foobar' status='WARNING']")
 println("##teamcity[message text='bibaboba' status='ERROR']")
 println("##teamcity[compilationFinished compiler='kotlinc']")
 
+println("##teamcity[message flowId='flow1' text='fooflow1']")
+println("##teamcity[message flowId='flow2' text='fooflow2']")
+
+println("##teamcity[message flowId='flow1' text='flow1bar']")
+println("##teamcity[message flowId='flow2' text='flow2bar']")
+
 var jarFile = File("find-plugin/build/libs/find.jar")
 if (jarFile.exists()) {
-    println("##teamcity[buildProblem description='Jar file with plugin was created successfully']")
+    println("##teamcity[message text='Jar file with plugin was created successfully']")
 } else {
     println("##teamcity[buildProblem description='Plugin could not be found']")
 }
