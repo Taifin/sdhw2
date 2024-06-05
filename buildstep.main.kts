d5c774dee88a19f703c0ffee96182b44839e6cce
Mikhail Rodionychev <tralllgm@gmail.com>
2024-06-05 10:10:20 +0200

    block in buildstep

--- buildstep.main.kts
+++ buildstep.main.kts
@@ -1,5 +1,10 @@
 import java.io.File
 
+println("##teamcity[blockOpened name='block1']")
+println("##teamcity[message text='foobar']")
+println("##teamcity[message text='bibaboba']")
+println("##teamcity[blockClosed name='block1']")
+
 var jarFile = File("find-plugin/build/libs/find.jar")
 if (jarFile.exists()) {
     println("##teamcity[buildProblem description='Jar file with plugin was created successfully']")
