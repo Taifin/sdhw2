d49c5e447310c6bb5f57eb91400eddb0e84a2fe9
Mikhail Rodionychev <tralllgm@gmail.com>
2024-06-06 11:12:06 +0200

    add inspection

--- buildstep.main.kts
+++ buildstep.main.kts
@@ -2,6 +2,20 @@ import java.io.File
 
 fun String.serviceMessage() = println("##teamcity[$this]")
 
+"inspectionType id='123' name='inspectionType' category='Style violations' description='<html>\n" +
+        "    <body>\n" +
+        "    Reports unnecessary local variables, which add nothing to the comprehensibility of a method. Variables caught include local variables which are immediately returned, local variables that are immediately assigned to another variable and then not used, and local variables which always have the same value as another\n" +
+        "    local variable or parameter.\n" +
+        "    <!-- tooltip end -->\n" +
+        "        <p>\n" +
+        "        Use the first checkbox below to have this inspection ignore variables which are immediately returned or thrown. Some coding styles suggest using such variables for clarity and ease of debugging.\n" +
+        "        </p>\n" +
+        "        <p>\n" +
+        "        Use the second checkbox below to have this inspection ignore variables which are annotated.\n" +
+        "        </p>\n" +
+        "    </body>\n" +
+        "</html>'".serviceMessage()
+
 println("##teamcity[compilationStarted compiler='kotlinc']")
 println("##teamcity[message text='foobar' status='WARNING']")
 println("##teamcity[message text='bibaboba' status='ERROR']")
@@ -17,6 +31,7 @@ println("##teamcity[message flowId='flow2' text='bar']")
 
 "testStarted name='test1'".serviceMessage()
 println("hahahahahaha")
+"inspection typeId='123' message='this is an inspection from test1' file='cli/src/main/kotlin/cub/taifin/App.kt' line='14' SEVERITY='WEAK WARNING'".serviceMessage()
 "testStdOut name='test1' out='text'".serviceMessage()
 "testStdErr name='test1' out='error text'".serviceMessage()
 "testFinished name='test1' duration='10000'".serviceMessage()
@@ -28,6 +43,7 @@ println("hihihihihihi")
 
 "testStarted name='test3'".serviceMessage()
 "testMetadata name='just some metadata' testName='test3' type='ms' value='434.5'".serviceMessage()
+"inspection typeId='123' message='this is an inspection from test3' file='.gitignore' line='1' SEVERITY='ERROR'".serviceMessage()
 "testFinished name='test3'".serviceMessage()
 
 "testStarted name='retry'".serviceMessage()
