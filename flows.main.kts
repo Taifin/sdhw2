274a2a19a934f41adcd428d7ada9202ff429216b
Mikhail Rodionychev <tralllgm@gmail.com>
2024-06-10 11:11:00 +0200

    flows and block

--- flows.main.kts
+++ flows.main.kts
@@ -2,11 +2,21 @@
 fun String.serviceMessage() = println("##teamcity[$this]")
 
 "flowStarted flowId='MainFlow'".serviceMessage()
+"blockOpened flowId='MainFlow' name='block1'".serviceMessage()
+
 "message flowId='MainFlow' text='message from MainFlow'".serviceMessage()
+
 "flowStarted flowId='SubFlow1' parent='MainFlow'".serviceMessage()
-"message flowId='SubFlow' text='message from SubFlow1'".serviceMessage()
+"blockOpened flowId='SubFlow1' name='subblock1'".serviceMessage()
+"message flowId='SubFlow1' text='message from SubFlow1'".serviceMessage()
+"blockClosed name='subblock1'".serviceMessage()
 "flowFinished flowId='SubFlow1'".serviceMessage()
-"flowStarted flowId='SubFlow2'".serviceMessage()
+
+"message flowId='MainFlow' text='another message from MainFlow'".serviceMessage()
+
+"flowStarted flowId='SubFlow2' parent='SubFlow1'".serviceMessage()
 "message flowId='MainFlow' text='message from SubFlow2'".serviceMessage()
 "flowFinished flowId='SubFlow2'".serviceMessage()
+
+"blockClosed name='block1'".serviceMessage()
 "flowFinished flowId='MainFlow'".serviceMessage()
